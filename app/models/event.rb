0d6fd44c4d3c768abccae4fda6bdae7c48460583
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2019-09-27 11:23:07 +0530

    [master] More checkpoint

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :allday, :presence: true
+
+  def seed_data row
+    Event.transaction do 
+      Event.create(title: row[0], startime: row[1], endtime: row[2], description: row[3], allday: row[5])
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  def seed_users row
+    User.create(username: row[0], email: row[1], phone: row[2])
+  end
 end
